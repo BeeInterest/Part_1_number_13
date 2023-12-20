3fe96107a4b39649bd52f75fd6fc119495a9f174
BeeInterest <kolosovasofya0@gmail.com>
2023-12-20 17:58:51 +0300

    add: func generate in class GeneratePassword

--- task.py
+++ task.py
@@ -6,3 +6,14 @@ class GeneratePassword:
     def __init__(self,length_password):
         alphabet = string.ascii_letters + string.digits + string.punctuation
         length_password = length_password
+
+    def generate(self):
+        try:
+            length_password = int(self.length_password)
+            if length_password < 8 or length_password > 32:
+                print('Введите цифру от 8 до 32')
+            else:
+                password= ''.join(secrets.choice(self.alphabet) for i in range(self.length_password))
+                print(f"length password: {self.length_password}, password: {password}")
+        except:
+            print('Введите цифру')
